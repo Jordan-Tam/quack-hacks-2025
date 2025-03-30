b52d87febc0037dfe410941410c400d53228f972
Your Name <111927619+thinktactics@users.noreply.github.com>
2025-03-30 00:53:22 -0400

    Added login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,67 @@
+import { useState } from 'react';
+
 const Login = () => {
-    return <h1>Login Page</h1>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:4000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setMessage(`OK: ${data.message}`);
+            } else {
+                setMessage(`Error: ${data.message}`);
+            }
+        } catch (err) {
+            console.error('Network error: ', err);
+            setMessage('Error.');
+        }
+    };
+
+    return (
+        <>
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <br />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label>Password:</label>
+                    <br />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <button type="submit">Login</button>
+
+                {message && <p>{message}</p>}
+            </form>
+        </>
+    );
 };
 
 export default Login;
